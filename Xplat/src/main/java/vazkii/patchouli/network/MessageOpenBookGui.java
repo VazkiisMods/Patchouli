f0df6548578d544702f42ad40325709f0362bb17
Minecraftschurli <minecraftschurli@gmail.com>
2024-08-24 20:28:21 +0200

    Refactor networking

--- Xplat/src/main/java/vazkii/patchouli/network/MessageOpenBookGui.java
+++ Xplat/src/main/java/vazkii/patchouli/network/MessageOpenBookGui.java
@@ -1,6 +1,7 @@
 package vazkii.patchouli.network;
 
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
@@ -10,27 +11,17 @@ import vazkii.patchouli.api.PatchouliAPI;
 import org.jetbrains.annotations.Nullable;
 
 public record MessageOpenBookGui(ResourceLocation book, @Nullable ResourceLocation entry, int page) implements CustomPacketPayload {
-
 	public static final ResourceLocation ID = ResourceLocation.fromNamespaceAndPath(PatchouliAPI.MOD_ID, "open_book");
-	public static final StreamCodec<FriendlyByteBuf, MessageOpenBookGui> CODEC = CustomPacketPayload.codec(
-			MessageOpenBookGui::write,
-			MessageOpenBookGui::new);
+	public static final StreamCodec<FriendlyByteBuf, MessageOpenBookGui> CODEC = StreamCodec.composite(
+			ResourceLocation.STREAM_CODEC,
+			MessageOpenBookGui::book,
+			ByteBufCodecs.STRING_UTF8.map(entry -> entry.isEmpty() ? null : ResourceLocation.tryParse(entry), entry -> entry == null ? "" : entry.toString()),
+			MessageOpenBookGui::entry,
+			ByteBufCodecs.VAR_INT,
+			MessageOpenBookGui::page,
+			MessageOpenBookGui::new
+	);
 	public static final Type<MessageOpenBookGui> TYPE = new Type<>(ID);
-
-	public MessageOpenBookGui(FriendlyByteBuf buf) {
-		this(buf.readResourceLocation(), getEntry(buf), buf.readVarInt());
-	}
-
-	private static ResourceLocation getEntry(FriendlyByteBuf buf) {
-		String entry = buf.readUtf();
-		return entry.isEmpty() ? null : ResourceLocation.tryParse(entry);
-	}
-
-	public void write(FriendlyByteBuf buf) {
-		buf.writeResourceLocation(book);
-		buf.writeUtf(entry == null ? "" : entry.toString());
-		buf.writeVarInt(page);
-	}
 
 	@Override
 	public Type<? extends CustomPacketPayload> type() {
